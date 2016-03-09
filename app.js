096d2996bdb4754eabad6ae0dc8b752da439d099
Michels <michelsa@oll43940mac-michels.ad.garmin.com>
2016-03-09 16:34:02 -0600

    got title working from video info object

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var fs = require('fs');
 var youtubedl = require('youtube-dl');
 
-var data;
-
+var url = 'https://www.youtube.com/watch?v=okF5gOTX9uM';
 var options = ['--extract-audio', '--prefer-ffmpeg', '--audio-format=mp3'];
-var video = youtubedl('https://www.youtube.com/watch?v=okF5gOTX9uM',
+var video = youtubedl(url,
   // Optional arguments passed to youtube-dl.
   options,
   // Additional options can be given for calling `child_process.execFile()`.
   { cwd: __dirname });
 
+
+
 // Will be called when the download starts.
-video.on('info', function(info) {
-
+video.on('info', function(info){
   console.log('Download started');
-
+  video.pipe(fs.createWriteStream('audio/'+info.title+'.mp3'));
 });
-
-video.pipe(fs.createWriteStream('audio/audio.mp3'));
