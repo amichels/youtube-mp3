4d67f46050cb2d1cebf4f7347ec233b1bb6969e1
Michels <michelsa@oll43940mac-michels.ad.garmin.com>
2016-03-09 12:37:31 -0600

    working on naming file on download

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var fs = require('fs');
 var youtubedl = require('youtube-dl');
-var filename;
+
+var data;
+
 var options = ['--extract-audio', '--prefer-ffmpeg', '--audio-format=mp3'];
 var video = youtubedl('https://www.youtube.com/watch?v=okF5gOTX9uM',
   // Optional arguments passed to youtube-dl.
   options,
   // Additional options can be given for calling `child_process.execFile()`.
-  { cwd: __dirname }); 
+  { cwd: __dirname });
+
 // Will be called when the download starts.
 video.on('info', function(info) {
-  filename = info.filename;
+
   console.log('Download started');
-  console.log('filename: ' + info.filename);
-  console.log('size: ' + info.size);
+
 });
 
-video.pipe(fs.createWriteStream('audio/'+filename+'.mp3'));
+video.pipe(fs.createWriteStream('audio/audio.mp3'));
