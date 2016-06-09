ef51eaaa44e9a368e52e254919448c80f710bfd4
amichels <andy.j.michels@gmail.com>
2016-06-08 22:49:28 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var fs = require('fs'),
-		youtubedl = require('youtube-dl'),
-		ffmpeg = require('fluent-ffmpeg'),
-		video = youtubedl('https://www.youtube.com/watch?v=okF5gOTX9uM');
+var mp3;
+
+var fs = require('fs');
+var	youtubedl = require('youtube-dl');
+var	ffmpeg = require('fluent-ffmpeg');
+var video = youtubedl('https://www.youtube.com/watch?v=okF5gOTX9uM');
 
 // Will be called when the download starts.
 video.on('info', function(info) {
   console.log('Download started');
   console.log('filename: ' + info._filename);
   console.log('size: ' + info.size);
+  mp3 = info._filename;
 });
 
 var proc = new ffmpeg({source:video});
 proc.setFfmpegPath('/Applications/ffmpeg/ffmpeg');
-proc.save('audio.mp3');
+proc.save(mp3 + '.mp3');
