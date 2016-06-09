d0cd4274de3af0e57d11743dbcf66673c8cce9e9
amichels <andy.j.michels@gmail.com>
2016-06-08 23:27:23 -0500

    working on getting filename

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-var mp3;
-
 var fs = require('fs');
 var	youtubedl = require('youtube-dl');
 var	ffmpeg = require('fluent-ffmpeg');
-var video = youtubedl('https://www.youtube.com/watch?v=okF5gOTX9uM');
+var url = 'https://www.youtube.com/watch?v=okF5gOTX9uM';
+var video = youtubedl(url);
+
+var mp3;
 
 // Will be called when the download starts.
 video.on('info', function(info) {
   console.log('Download started');
-  console.log('filename: ' + info._filename);
-  console.log('size: ' + info.size);
-  mp3 = info._filename;
 });
 
 var proc = new ffmpeg({source:video});
